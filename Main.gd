4e204b870bbfe36ac8187a25e48491815fd04693
im-mhid <hmatheus031@gmail.com>
2021-05-01 18:20:19 -0300

    Animações, reiniciar, pontos e record

--- Main.gd
+++ Main.gd
@@ -1,8 +1,26 @@
 extends Node2D
-var velocidade = Vector2(-500, 0)
+
+var velocidade = Vector2(-700 , 0)
+var velocidade_inicial = velocidade
+var offset_inicial = Vector2(0 , 0)
+var comecou = false
+var acabou = false
+var pontuacao_obstaculos = 10
+var pontos = 0
+var record = 0
 
 func _ready():
+	get_node("Ollie").connect("recomecou", self, "quando_recomecou")
 	pass
 
 func _process(delta):
+	if not comecou:
+		return
+		
 	velocidade.x -= delta / 5
+
+func quando_recomecou():
+	velocidade = velocidade_inicial
+	$ParallaxBackground.set_scroll_offset(offset_inicial)
+	pontos = 0
+	get_node("Pontos").text = "Pontos: " + str(pontos)
--- Ollie.gd
+++ Ollie.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal recomecou
+
 var chao = Vector2(120,430)
 var gravidade = 4000
 var velocidade = Vector2()
@@ -20,9 +22,13 @@ var obstaculos = [preload("res://Hidrante.tscn"),
 func _ready():
 	set_position(chao)
 	randomize()
+	$AnimatedSprite.play("parado")
 	pass
 
 func _physics_process(delta):
+	if not get_parent().comecou:
+		return
+	
 	tempo += delta
 	
 	if tempo >= intervalo_ini:
@@ -37,11 +43,13 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("pular"):
 		velocidade.y += gravidade * delta
+		$AnimatedSprite.play("pulo")
 	else:
 		velocidade.y += gravidade * delta * modificador_gravidade
 	
 	if Input.is_action_just_pressed("pular") and position == chao:
 		velocidade.y = velocidade_pulo
+		$AnimatedSprite.play("pulo")
 	
 	position += velocidade * delta
 	
@@ -51,4 +59,21 @@ func _physics_process(delta):
 		velocidade = Vector2()
 		
 func colidiu(area):
-	queue_free()
+	$AnimatedSprite.play("morte")
+	get_parent().comecou = false
+	get_parent().acabou = true
+	
+	if get_parent().pontos > get_parent().record:
+		get_parent().record = get_parent().pontos
+	pass
+	
+func _input(event):
+	if event is InputEventKey or event is InputEventMouseButton:
+		if get_parent().acabou and not event.is_echo():
+			emit_signal("recomecou")
+			get_parent().acabou = false
+			get_parent().get_node("Record").text = "Record: " + str(get_parent().record)
+			pass
+			
+		get_parent().comecou = true
+		$AnimatedSprite.play("andando")
